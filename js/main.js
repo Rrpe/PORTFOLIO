612fc280706fac9e63d6de6e92d004125554dba9
DESKTOP-QM1776V\kel_7 <kelobelo37@gmail.com>
2020-08-04 21:39:29 +0900

    RE

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-$(function() {
+$(function () {
     console.log("Connection...");
+
+
+    setTimeout(function () {
+
+        window.open("about:blank","_self").close();
+
+    }, 600000);
 
     var $btn = $(".btn");
 
@@ -9,21 +16,51 @@ $(function() {
 
 
     // 메뉴 트리거
-    $btn.on("click", function() {
-        $(this).addClass("on");
+    $btn.on("click", function () {
+        $btn.addClass("on").hide();
         $menu.addClass("on").fadeIn(500);
     });
 
-    $menu_cancel.on("click", function() {
+    $menu_cancel.on("click", function () {
         $menu.removeClass("on").hide();
-        $btn.removeClass("on");
-    }); 
+        $btn.removeClass("on").show();
+    });
 
     var li_index = $(".menu .container ul li");
 
-    li_index.on("click", function(){
+    li_index.on("click", function () {
         $menu.removeClass("on").hide();
         $btn.removeClass("on");
     }); // 메뉴 트리거
-});
 
+    // work 슬라이드 이벤트
+    $(".work .container").slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        swipeToSlide: true
+    });
+
+    // 슬라이드 이미지 호버
+    var $work_content = $(".work .container .card .imgbox .content");
+    $work_content.hide();
+    $(".work .imgbox").on("mouseenter", function () {
+        $(this).find(".content").show();
+        $(this).find(".content").stop().animate({
+            "bottom": "0px"
+        });
+        // $(this).stop().animate({
+        //     'bottom': 0
+        // }, 1000);
+    }).on("mouseleave", function () {
+        $(this).find(".content").slideDown();
+        $(this).find(".content").stop().animate({
+            "bottom": "-250px"
+        });
+    });
+});
